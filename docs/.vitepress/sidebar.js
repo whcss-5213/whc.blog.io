ef0af27520d272c9e4a299d3f4aa6a5e8882ba9a
W Hc <2889597857@qq.com>
2025-06-05 00:29:34 +0800

    color

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -74,7 +74,8 @@ export default {
             { text: 'layout', link: '/CSS/layout' },
             { text: 'text', link: '/CSS/text' },
             { text: 'unit', link: '/CSS/unit' },
-            { text: 'cursor', link: '/CSS/cursor' }
+            { text: 'cursor', link: '/CSS/cursor' },
+            { text: 'Color', link: '/CSS/color' },
         ],
     },
     '/git/': {
