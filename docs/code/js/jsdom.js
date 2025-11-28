b4e666208717039abc540cfbfc2849a67aaae723
W Hc <2889597857@qq.com>
2025-11-29 03:08:48 +0800

    VirtualConsole

--- docs/code/js/jsdom.js
+++ docs/code/js/jsdom.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { JSDOM } from 'jsdom';
+import { JSDOM, VirtualConsole } from 'jsdom';
 import jschardet from 'jschardet';
 import iconv from 'iconv-lite';
 
@@ -14,8 +14,18 @@ const encoding = jschardet.detect(response.data).encoding;
 // 编码转换
 const html = iconv.decode(response.data, encoding);
 
+// 创建虚拟窗口
+// 屏蔽控制台输出
+const virtualConsole = new VirtualConsole();
+virtualConsole.on('error', () => {});
+virtualConsole.on('warn', () => {});
+virtualConsole.on('info', () => {});
+virtualConsole.on('dir', () => {});
+virtualConsole.on('jsdomError', () => {});
+
 const { window } = new JSDOM(html, {
   runScripts: 'dangerously',
+  virtualConsole,
 });
 
 const document = window.document;
